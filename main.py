42af4ecc4c8e146878f212b60b74d9f43e85ded0
Johann Schneider <joschi.dev7@gmail.com>
2025-07-01 10:31:09 +0200

    gamelogic has been added to the main.py file.

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))  # Clear the screen with black
+
+        pygame.display.flip()  # Update the display
+
+    """
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+    """
 if __name__ == "__main__":
     main()
